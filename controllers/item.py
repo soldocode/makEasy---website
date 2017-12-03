1c1d50e2c874baeee7e909a6792e16c0749ad3ae
soldocode <riccardo.soldini@gmail.com>
2017-12-03 09:01:17 +0000

    Modified estimate controller and view

--- controllers/item.py
+++ controllers/item.py
@@ -7,6 +7,7 @@ import cStringIO
 import sys, os
 import jsonpickle
 import g2
+import requests
 
 pathname = os.path.abspath(os.path.dirname(sys.argv[0]))
 sysfolder=pathname.split('web2py')[0]
@@ -51,6 +52,54 @@ def create():
 
 def estimate():
     return dict(locals())
+
+
+def sendOffer():
+    # get parameters
+    #item=json.loads(request.vars.item)
+    prj_name=request.vars.prj_name
+    json_prj_data=request.vars.prj_data
+    prj_data=json.loads(json_prj_data)
+    json_offer=request.vars.offer
+    offer=json.loads(json_offer)
+
+    #create items
+    item=makEasy.newItemFromProject(prj_name,prj_data)
+
+    #get project data
+    prj=makEasy.projectLibrary[prj_name]
+    prj_path=prj.Path
+
+    f = open(prjfolder+prj_path+'/form.json', 'r')
+    prj_form=json.load(f)
+    f.close()
+
+    html=''
+    html+="<p>"
+    html+="Buongiorno,<br>Vi inviamo offerta per la fornitura di:<br><br>"
+    html+="Nr "+str(offer['data_form']['quantity'])+" "
+    html+=item.Project.Title
+    html+=" in "+item.ClassProperties['Material']
+    html+=" sp " + str(item.ClassProperties['Thickness'])+"mm"
+    html+=" con le seguenti dimensioni:<br>"
+    for field in prj_form['form_data']:
+        if 'label' in field:
+            html+="> "+field['label']+": "+"<br>"
+    #html+=json_prj_data
+    #html+=json_offer
+    html+="</p>"
+    result=requests.post(
+        "https://api.mailgun.net/v3/carpenteriasoldini.it/messages",
+        auth=("api", "key-88b2ca18a8d06347d6f9fd35b7fe8a07"),
+        data={"from": "SOLDINI snc - Offerte <info@carpenteriasoldini.it>",
+              "to": "produzione@carpenteriasoldini.it",
+              "subject": "Offerta",
+              "html": html})
+
+    re="Offerta inviata"
+    return json.dumps(dict(msg=result.reason))
+
+
 
 
 def exportDXF():
